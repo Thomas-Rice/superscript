2b38f6c7442eb3b95035ffe02e2287e2437c6426
Thomas-Rice <tikkleanim@gmail.com>
2016-03-17 14:29:51 +0000

    Added files via upload

--- test_widget.py
+++ test_widget.py
@@ -25,6 +25,7 @@ class BlockWidget(QtGui.QWidget):
 		checkBoxLayout = QtGui.QVBoxLayout()
 		checkBoxLayout.setContentsMargins(0,0,0,0)
 		for name in self.names:
+			# print name
 			checkBox = QtGui.QCheckBox(name)
 			self.checkboxes[name] = checkBox
 			checkBoxLayout.addWidget(checkBox)
@@ -101,7 +102,7 @@ class Container(QtGui.QWidget):
 		self.setLayout(main_layout)
 
 	def create_connections(self):
-		self.button.clicked.connect(self.return_configs)
+		self.button.clicked.connect(self.make_final_dictionary)
 
 
 	def populate(self, config = None):
@@ -131,30 +132,26 @@ class Container(QtGui.QWidget):
 			returned_list = self.table.cellWidget(widget,0).return_checked()
 			if not returned_list[1] == []:
 				config_list.append(returned_list)
-		# print config_list
-		self.make_final_dictionary(config_list)
+		# self.make_final_dictionary(config_list)
 		return config_list
 
-	def make_final_dictionary(self,final_dictionary):
-		final_build = []
-		final_config = [] 
-		# self.return_configs()
+
+	def make_final_dictionary(self):
+		final_dictionary = self.return_configs()
+		config_dictionary = {}
+		build_list = []
 		with open('jenkins_Builds.json') as data_file:
 			original_dict = json.load(data_file)
 
-		for element in final_dictionary:
-			for i in range(len(element[1])):
-				tmp = []
-				print original_dict['Modo']['Joota_901dev']
-				# final_build.append(element)
-				# for j in original_dict[element[0][0]][element[1][i]]:
-				# 	# print j
-				# 	tmp.append(j)
-				# final_config.append(tmp)
+		#get chosen builds and their configs then make that into a dictionary
+		for item in final_dictionary:
+			for l in item[1]:
+				config_dictionary[l] = original_dict[item[0][0]][l]
+				build_list.append(l)
+		final_list = [build_list,config_dictionary]
+		# print config_dictionary
+		return final_list
 
-		# for t in range(len(final_build)):
-		# 	print final_build[t]
-		# 	print final_config[t]
 
 
 
@@ -200,11 +197,11 @@ class main_window(QtGui.QWidget):
 
 	def create_connections(self):
 		self.button.clicked.connect(self.change_Layout)
+		self.button2.clicked.connect(self.generate_config_window)
 
 	def change_Layout(self):
 		if self.layout == "First_Page":
 
-			self.product_form.return_checked()
 			#get the builds from jenkins for each product
 			product_and_config = self.jenkins_object.populate_products_with_builds(self.products)
 			#Populate the checkboxes
@@ -214,9 +211,9 @@ class main_window(QtGui.QWidget):
 			self.layout = 'Second_Page'
 
 		else:
-			self.product_form.return_checked()
 			product_and_config = self.jenkins_object.populate_products_with_builds(self.products)
-			#Populate the checkboxes
+			#Populate the checkboxes with product names and a dictionary of the builds
+			# print product_and_config[1]
 			self.checkbox_form = Container(product_and_config[0],product_and_config[1])
 
 			self.splitter.widget(1).hide()
@@ -224,9 +221,18 @@ class main_window(QtGui.QWidget):
 			self.layout = 'Second_Page'
 
 	def generate_config_window(self):
+		formatted_config_list = []
+		build_list = []
+		config_dictionary = self.checkbox_form.make_final_dictionary()
+		formatted_config_list.append(config_dictionary[1])
+		for each in config_dictionary[1]:
+			build_list.append(each)
+			formatted_config_list.append(config_dictionary[1][each]) 
+		del formatted_config_list[0]
+		self.config_checbox_form = Container(build_list,formatted_config_list)
+		self.resize(800,self.height)
+		self.splitter.addWidget(self.config_checbox_form)
 
-		self.config_checkbox_form = Container(product_and_config[0],product_and_config[1])
-		self.splitter.addWidget()
 
 
 
@@ -284,14 +290,14 @@ class choose_projects():
 	def get_product_list(self):
 		# #until i get something working with this it will have to be manual OR I can do a dictionary where the user can add to it
 
-		# products = ['Mari','Modo']
-		products = ['Collectives','Colourway','Core','Gonzo',"HPC",'Katana','Licensing','Live','MVC','Mari','Mischief','Modo','Nuke','Playground','Research']
+		products = ['Mari','Modo','Nuke']
+		#products = ['Collectives','Colourway','Core','Gonzo',"HPC",'Katana','Licensing','Live','MVC','Mari','Mischief','Modo','Nuke','Playground','Research']
 		return products
 
 	def populate_products_with_builds(self,product_list):
 		self.config = []
-		# ins = getProjectConfigurations()
-		# ins.return_multiple_projects(product_list)
+		ins = getProjectConfigurations()
+		ins.return_multiple_projects(product_list)
 
 		with open('jenkins_Builds.json') as data_file:
 			test1 = json.load(data_file)
@@ -300,6 +306,7 @@ class choose_projects():
 		for each in test1:
 			self.products.append(each)
 			self.config.append(test1[each])
+
 
 		combined_list = [self.products, self.config]
 		return combined_list
